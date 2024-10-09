e008d3256c81269b2a594a9a1a51695e91bddad3
Goulin Khoge <36013798+goulinkh@users.noreply.github.com>
2024-10-09 17:20:13 +0200

    avoid multi-lines exception in production logs

--- app/logging.py
+++ app/logging.py
@@ -1,11 +1,27 @@
 import logging
+from types import TracebackType
+
+from uvicorn.logging import DefaultFormatter
+
+
+class CustomLogsFormatter(DefaultFormatter):
+    def format(self, record):
+        formatted = super().format(record)
+        return formatted.replace("\n", "\\n")
 
 
 def setup_logging():
     log_format = "%(asctime)s [%(levelname)s] (%(name)s:%(module)s) %(message)s"
-
-    logger = logging.getLogger("uvicorn.access")
     handler = logging.StreamHandler()
-    handler.setFormatter(logging.Formatter(log_format))
-    logger.handlers = [handler]
+    handler.setFormatter(CustomLogsFormatter(fmt=log_format))
+
+    root_logger = logging.getLogger()
+    root_logger.handlers = [handler]
+
+    uvicorn_access_logger = logging.getLogger("uvicorn.access")
+    uvicorn_access_logger.handlers = [handler]
+
+    uvicorn_error_logger = logging.getLogger("uvicorn.error")
+    uvicorn_error_logger.handlers = [handler]
+
     logging.basicConfig(level=logging.INFO, format=log_format, handlers=[handler])
