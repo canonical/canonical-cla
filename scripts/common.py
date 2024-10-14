464b3198d56bfc06a8adb255876421f9c0c50d03
Goulin Khoge <36013798+goulinkh@users.noreply.github.com>
2024-10-14 16:58:09 +0200

    add import script

---
+++ scripts/common.py
@@ -0,0 +1,59 @@
+import inspect
+from contextlib import AsyncExitStack
+from typing import Any, Callable
+
+from fastapi import Request
+from fastapi.datastructures import State
+from fastapi.dependencies.utils import get_dependant, solve_dependencies
+
+
+async def run_command(command: Callable[..., Any], *args: Any, **kwargs: Any) -> Any:
+    """Given a callable, will solve its dependencies and run it with provided args and kwargs."""
+    if len(args) > 0:
+        raise ValueError("Positional arguments are not supported")
+    async with AsyncExitStack() as cm:
+        query = {}
+        for key, value in kwargs.items():
+            query[key] = value
+
+        query_string = "&".join([f"{key}={value}" for key, value in query.items()])
+
+        request = Request(
+            {
+                "type": "http",
+                "headers": [],
+                "query_string": query_string.encode(),
+                "fastapi_astack": cm,
+                "state": State(),
+            }
+        )
+
+        dependant = get_dependant(path="command", call=command)
+
+        (
+            solved_kwargs,
+            errors,
+            background_tasks,
+            sub_concurrency_limits,
+            dependency_overrides_provider,
+        ) = await solve_dependencies(
+            request=request,
+            dependant=dependant,
+            body=None,
+            background_tasks=None,
+            response=None,
+            dependency_overrides_provider=None,
+            async_exit_stack=cm,
+        )
+
+        if errors:
+            raise Exception(f"Dependency solving errors: {errors}")
+        if not dependant.call:
+            raise ValueError("Could not find a callable to run")
+
+        if inspect.iscoroutinefunction(dependant.call):
+            result = await dependant.call(**solved_kwargs)
+        else:
+            result = dependant.call(**solved_kwargs)
+
+        return result
---
+++ scripts/import_contributors.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+"""
+A CLI script to be run with Bash to import contributors from a JSON file.
+Make sure environment variables are set before running this script.
+"""
+
+import argparse
+import asyncio
+import datetime
+import json
+
+from fastapi import Depends
+
+from app.database.models import Individual
+from app.repository.individual import IndividualRepository, individual_repository
+from scripts.common import run_command
+
+
+async def import_contributors(
+    file_path: str,
+    individual_repository: IndividualRepository = Depends(individual_repository),
+):
+    """
+    Import contributors from a JSON file.
+    """
+    with open(file_path, "r") as file:
+        contributors = json.load(file)
+
+    imported_count = 0
+    print("Importing contributors...")
+    for contributor in contributors:
+        contributor["signed_at"] = datetime.datetime.strptime(
+            contributor["date"], "%Y-%m-%dT%H:%M:%S.%fZ"
+        ).replace(tzinfo=None)
+        email_field_name = (
+            "github_email" if contributor.get("github_username") else "launchpad_email"
+        )
+        contributor[email_field_name] = contributor["email"]
+        contributor.pop("email")
+        contributor.pop("date")
+        try:
+
+            individual = Individual(**contributor)
+            await individual_repository.create_individual(individual)
+            imported_count += 1
+        except Exception as e:
+            print(f"Failed to import contributor {contributor[email_field_name]}: {e}")
+    print(f"Imported {imported_count} contributors.")
+
+
+def main():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument(
+        "--input",
+        help="JSON file with contributors",
+        type=str,
+        required=True,
+    )
+    args = arg_parser.parse_args()
+    file_path = args.input
+
+    asyncio.run(run_command(import_contributors, file_path=file_path))
+
+
+if __name__ == "__main__":
+    main()
