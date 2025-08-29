cffae8a58d46bf064426f6e052b5a4702a3eb853
Goulin Khoge <36013798+goulinkh@users.noreply.github.com>
2025-08-29 05:57:30 +0200

    chore: format

--- migrations/versions/68bfaf39538c_ind_remove_phone_number.py
+++ migrations/versions/68bfaf39538c_ind_remove_phone_number.py
@@ -9,18 +9,21 @@ Create Date: 2025-08-22 11:35:17.704461
 import sqlalchemy as sa
 from alembic import op
 
-
 # revision identifiers, used by Alembic.
-revision = '68bfaf39538c'
-down_revision = '47c1575bd0b8'
+revision = "68bfaf39538c"
+down_revision = "47c1575bd0b8"
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
-    op.drop_column('individual', 'phone_number')
+    op.drop_column("individual", "phone_number")
 
 
 def downgrade():
-    op.add_column('individual', sa.Column('phone_number', sa.VARCHAR(
-        length=20), autoincrement=False, nullable=False))
+    op.add_column(
+        "individual",
+        sa.Column(
+            "phone_number", sa.VARCHAR(length=20), autoincrement=False, nullable=False
+        ),
+    )
