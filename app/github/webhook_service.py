718b93a1526448c8df2a66a4e602dd3b59929232
Maksim Beliaev <beliaev.m.s@gmail.com>
2025-10-15 18:27:42 +0300

    add hash validation

--- app/github/webhook_service.py
+++ app/github/webhook_service.py
@@ -1,6 +1,8 @@
+import hashlib
+import hmac
 import re
 
-from fastapi import Depends, Response
+from fastapi import Depends, HTTPException, Response
 from github import Github, GithubIntegration
 
 from app.cla.service import CLAService, cla_service
@@ -45,6 +47,36 @@ def get_github_client(owner: str, repo_name: str):
 class GithubWebhookService:
     def __init__(self, cla_service: CLAService):
         self.cla_service = cla_service
+
+    def verify_signature(
+        self, payload_body: bytes, signature_header: str | None
+    ):
+        """Verify that the payload was sent from GitHub by validating SHA256.
+
+        Raise and return 403 if not authorized.
+
+        Args:
+            payload_body: original request body to verify (request.body())
+            signature_header: header received from GitHub (x-hub-signature-256)
+
+        """
+        if not signature_header:
+            raise HTTPException(
+                status_code=403, detail="x-hub-signature-256 header is missing!"
+            )
+
+        hash_object = hmac.new(
+            config.github_app.secret.get_secret_value().encode(
+                "utf-8"
+            ),
+            msg=payload_body,
+            digestmod=hashlib.sha256,
+        )
+        expected_signature = "sha256=" + hash_object.hexdigest()
+        if not hmac.compare_digest(expected_signature, signature_header):
+            raise HTTPException(
+                status_code=403, detail="Request signatures didn't match!"
+            )
 
     async def process_webhook(self, payload: dict):
         """
